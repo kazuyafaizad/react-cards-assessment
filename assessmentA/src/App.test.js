e8a40735060b0f67fea98bbe939f4092336ce975
kazuyafaizad <kazuyafaizad@gmail.com>
2022-09-01 00:45:31 +0800

    make test

--- assessmentA/src/App.test.js
+++ assessmentA/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { getAllByDisplayValue, render, screen, queryByText } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+import Button from "./Button";
 
-test('renders learn react link', () => {
+test("renders enter number player", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Please enter number of player/i)).toBeInTheDocument();
+});
+
+test("click distribute card", () => {
+  render(<App numberOfPlayers={4} />);
+  const button = screen.queryByText("DISTRIBUTE CARD");
+
+  const input = screen.getByDisplayValue("");
+  userEvent.type(input, "4");
+  expect(input).toHaveDisplayValue(4);
+
+  userEvent.click(button);
 });
